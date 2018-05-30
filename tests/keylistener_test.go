7458ffbfcc90bee73e1a8c7bcbf94c77bc906e84
kmilinho <c.pereira.leon@gmail.com>
2018-05-30 00:29:41 -0400

    Test improvements. Now injecting test events to be returned by the term controller Poll method

--- tests/keylistener_test.go
+++ tests/keylistener_test.go
@@ -2,76 +2,56 @@ package tests
 
 import (
 	"testing"
-	"container/list"
 	"github.com/kmilinho/twcli/pkg/keys"
 	"time"
-	"fmt"
 )
 
-func TestListenerWithTermController(t *testing.T) {
-	mockTermController := &TestTermController{t, list.New()}
+func TestListenerProcessTermControllerPolledEvents(t *testing.T) {
+	var expectedCharFromEvent rune = 97
+
+	mockTermController := &TestTermController{t,
+		keys.KeyEvent{Char: expectedCharFromEvent},
+	}
+
 	listener := keys.NewKeyListener(mockTermController)
-	listener.Register("x", func(s string) {})
+	listener.Register("a", func(s string) {
+		receivedCharFromEvent := []rune(s)[0]
+		if receivedCharFromEvent != expectedCharFromEvent {
+			t.Error("expected=" + string(expectedCharFromEvent) + " but received=" + string(receivedCharFromEvent))
+		}
+	})
 	listener.Start()
 	go func() {
 		time.Sleep(time.Second)
 		listener.Stop()
-		t.Log("listener stopped")
 	}()
 	listener.Wait()
-
-	for e := mockTermController.interaction.Front(); e != nil; e = e.Next() {
-		fmt.Println(e.Value)
-	}
-}
-
-func TestRegisterFailsIfListenerIsRunning(t *testing.T) {
-}
-
-func TestRegisterDoesNotFailIfListenerIsStopped(t *testing.T) {
-	listener := keys.NewKeyListener(&TestTermController{})
-	listener.Start()
-	listener.Stop()
-
-	k, err := listener.Register("x", func(s string) {
-
-	})
-
-	if k == nil || err != nil {
-		t.Errorf("Register() must not fail after stopping the listener")
-	}
 }
 
 type TestTermController struct {
-	t *testing.T
-	interaction *list.List
+	t     *testing.T
+	event keys.KeyEvent
 }
 
 func (tc *TestTermController) GetErrorEventType() uint8 {
-	tc.interaction.PushBack("GetErrorEventType")
 	return 1
 }
 
 func (tc *TestTermController) GetKeyEventType() uint8 {
-	tc.interaction.PushBack("GetKeyEventType")
 	return 0
 }
 
-func (tc *TestTermController) Init() error{
-	tc.interaction.PushBack("Init")
+func (tc *TestTermController) Init() error {
 	return nil
 }
 
 func (tc *TestTermController) Close() {
-	tc.interaction.PushBack("Close")
 }
 
 func (tc *TestTermController) PollEvent() keys.KeyEvent {
-	tc.interaction.PushBack("PollEvent")
 	time.Sleep(200 * time.Millisecond)
-	return keys.KeyEvent{}
+	return tc.event
 }
 
 func (tc *TestTermController) Sync() {
-	tc.interaction.PushBack("Sync")
 }
